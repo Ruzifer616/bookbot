63a30d597519d0bfdaff8c4d454bd8d3de8c3baf
Ruzifer616 <ruzifer98@gmail.com>
2025-06-11 16:18:58 +0200

    chapter 3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import *
 
 def get_book_text(file_path):
@@ -5,10 +6,23 @@ def get_book_text(file_path):
         return f.read()
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book = get_book_text(sys.argv[1])
     num_words = get_num_words(book)
-    characters = get_all_symbols(book)
-    print(f"{num_words} words found in the document")
-    print(characters)
+    characters = get_all_characters(book)
+    characters = get_sorted_characters(characters)
+
+
+    print("========= BOOKBOT =========")
+    print("Analyzing book found at books/frankenstein.txt")
+    print("-------- Word Count -------")
+    print(f"Found {num_words} total words")
+    print("----- Character Count -----")
+    for d in characters:
+        print(f"{d["char"]}: {d["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -2,13 +2,28 @@ def get_num_words(book: str):
     words = book.split()
     return len(words)
 
-def get_all_symbols(book: str):
+def get_all_characters(book: str):
     characters = {}
 
     for char in book:
+        if char == " ":
+            continue
         if char.lower() in characters:
             characters[char.lower()] = characters[char.lower()]+1
         else:
             characters[char.lower()] = 1
 
     return characters
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_characters(characters_in):
+
+    characters_out = []
+    for char in characters_in:
+        characters_out.append({"char": char, "num": characters_in[char]})
+
+    characters_out.sort(reverse=True, key=sort_on)
+
+    return characters_out
